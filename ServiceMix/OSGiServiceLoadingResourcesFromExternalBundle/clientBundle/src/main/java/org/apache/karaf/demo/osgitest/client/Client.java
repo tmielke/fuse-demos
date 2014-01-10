5ecc6dd8da6622b0c9dd2f3e45c88f35586722df
Torsten Mielke <tmielke@redhat.com>
2014-01-10 17:27:56 +0100

    Simplified OSGiServiceLoadingResourcesFromExternalBundle so it does not require OSGi Bundle* classes

--- ServiceMix/OSGiServiceLoadingResourcesFromExternalBundle/clientBundle/src/main/java/org/apache/karaf/demo/osgitest/client/Client.java
+++ ServiceMix/OSGiServiceLoadingResourcesFromExternalBundle/clientBundle/src/main/java/org/apache/karaf/demo/osgitest/client/Client.java
@@ -4,9 +4,6 @@ import org.apache.karaf.demo.osgitest.OSGiTestInterface;
 
 import java.net.URL;
 
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.Bundle;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,58 +15,28 @@ import org.slf4j.LoggerFactory;
 public class Client {
 
     private Logger LOG = LoggerFactory.getLogger(Client.class);
-    private BundleContext context = null;
+    private OSGiTestInterface service = null;
 
-
-    /* (non-Javadoc)
-     * @see com.fusesource.test.osgitest.OSGiTestInterface#doIt()
-     */
     public void doIt() {
-        LOG.info("OSGiTestImpl.doIt() invoked.");
+        LOG.info("Client.doIt() invoked.");
+        URL url = this.getClass().getClassLoader().getResource("OSGI-INF/blueprint/blueprint.xml");
+        LOG.info("url is " + url);
+
+        // Invoke the service, pass in the resource url
+        service.invokeService(url.toString());
+        LOG.debug("service() called succesfully.");
         LOG.debug("Done with doIt()");
     }
 
 
     /**
-     * Injects the OSGi service proxy.
+     * Injects the OSGi service proxy and directly invokes the service
+     * (invocation for testing purposes).
      * @param service
      */
     public void setService(OSGiTestInterface service) {
         LOG.debug("setService() invoked.");
-
-        //ensure the bundle context is injected before proceeding
-        if (context == null) {
-            LOG.error("BundleContext not set");
-            return;
-        }
-        // Use the BundleContext to get a ref to this OSGi bundle
-        URL url = null;
-        try {
-            Bundle bundle = context.getBundle();
-            
-            // Use Bundle.getResource() to create a url of the form
-            // bundle:[id].[rev]/[path]
-            // pointing to the resource file that is then passed to the 
-            // OSGi service invocation.
-            url = bundle.getResource("OSGI-INF/blueprint/blueprint.xml");
-
-            LOG.info("url is " + url);
-        } catch (Exception ex) {
-            LOG.error(ex.getMessage());
-        }
-        // Invoke the service, pass in the resource url
-        service.invokeService(url.toString());
-        LOG.debug("service() called succesfully.");
-    }
-
-
-    /**
-     * Sets the OSGi BundleContext. Its needed to 
-     * create the resource url.
-     * @param context
-     */
-    public void setContext(BundleContext context) {
-        LOG.debug("setContext() invoked.");
-        this.context = context;
+        this.service = service;
+        doIt();
     }
 }
